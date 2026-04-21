4714d483f6dee4b92679d6f949f5ec0ca3b5a0e9
Miguel300l <miguelledezma400@gmail.com>
2026-04-21 13:34:06 -0500

    Carga imagen mas rapido nosotros

--- src/landing/pages/Nosotros.jsx
+++ src/landing/pages/Nosotros.jsx
@@ -45,6 +45,9 @@ const Nosotros = () => {
           src={nosotrosBarber}
           className="position-absolute top-0 start-0 w-100 h-100 img-fade"
           alt="nosotrosBarber"
+          decoding="async"
+          loading="eager"
+          fetchpriority="high"
         />
 
         {/* Capa oscura */}
